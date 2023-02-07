e30a5f9efdd848dfe2869b5bc2d826c6f7b57b31
MiguelSperle <miguelsperle205@yahoo.com>
2023-02-07 19:21:24 -0300

    Atualizado

--- src/Providers/useProducts.jsx
+++ src/Providers/useProducts.jsx
@@ -1,87 +1,94 @@
 // contextAPI
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useLocalStorage from "use-local-storage"; // lib de local storage( salvar no navegador quando algo é adicionado)
+
 export const ProductsContext = React.createContext();
+
 export const ProductsProvider = ({ children }) => {
-  
-  const [products, setProducts] = useState([]);
+  const productsLocalStorage = JSON.parse(localStorage.getItem('products') || '[]')
+
+  const [products, setProducts] = useState(productsLocalStorage);
+
+  useEffect(() => {
+    localStorage.setItem('products', JSON.stringify(products))
+  }, [products])
 
 
 
-function addCart({
-      id,
-      name,
-      price,
-      image,
-      amountBuy
-    }){
-        const verificationExists = products.find((product) => product.id === id)
-        if(verificationExists){
-          return alert('item adicionado já no seu carrinho.')
-        }
-        setProducts([...products ,{ id, name, price, image, amountBuy}])
-       
+
+  function addCart({
+    id,
+    name,
+    price,
+    image,
+    amountBuy
+  }) {
+    const verificationExists = products.find((product) => product.id === id)
+    if (verificationExists) {
+      return alert('item adicionado já no seu carrinho.')
     }
+    setProducts([...products, { id, name, price, image, amountBuy }])
+  }
 
-function addAmountItem({
-      id,
-    }){
-      const itemExists = products.find((item) => item.id === id)
-      const newItem = {
-        ...itemExists,
-        amountBuy: itemExists.amountBuy + 1,   
+  function addAmountItem({
+    id,
+  }) {
+    const itemExists = products.find((item) => item.id === id)
+    const newItem = {
+      ...itemExists,
+      amountBuy: itemExists.amountBuy + 1,
     }
 
     setProducts(products.map((item) => {
-      if(item.id === id) {
-          return newItem
-          
+      if (item.id === id) {
+        return newItem
+
       } else {
         return {
           ...item
         }
       }
-      
-     }))
-      
+
+    }))
+
+  }
+
+  function removeAmountItem({
+    id
+  }) {
+    const itemExists = products.find((item) => item.id === id)
+    const newItem = {
+      ...itemExists,
+      amountBuy: itemExists.amountBuy - 1,
     }
 
-function removeAmountItem({
-      id
-    }){
-      const itemExists = products.find((item) => item.id === id)
-      const newItem = {
-        ...itemExists,
-        amountBuy: itemExists.amountBuy - 1,   
-      }
+    if (itemExists.amountBuy <= 1) {
+      return itemExists.amountBuy + 1
+    }
 
-      if(itemExists.amountBuy <= 1){
-        return itemExists.amountBuy + 1
-      }
+    setProducts(products.map((item) => {
+      if (item.id === id) {
+        return newItem
 
-      setProducts(products.map((item) => {
-        if(item.id === id) {
-            return newItem
-            
-        } else {
-          return {
-            ...item
-          }
+      } else {
+        return {
+          ...item
         }
-        
-       }))
-      
+      }
+
+    }))
+
+  }
+
+
+  function RemoveProductsCart(idProduct) {
+    const Restantes = products.filter((product) => product.id !== idProduct)
+
+    if (RemoveProductsCart) {
+      setProducts(Restantes)
     }
+  }
 
-
-function RemoveProductsCart(idProduct){
-      const Restantes = products.filter( (product) => product.id !== idProduct) 
-
-      if(RemoveProductsCart){
-        setProducts(Restantes)
-      }                                    
-    }
-  
 
 
   return (
