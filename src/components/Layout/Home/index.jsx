bdaa53c9e1923eba59d78858612d42c132b4a750
MiguelSperle <miguelsperle205@yahoo.com>
2023-02-07 20:35:47 -0300

    Atualizado

--- src/components/Layout/Home/index.jsx
+++ src/components/Layout/Home/index.jsx
@@ -12,7 +12,8 @@ function Home(){
           <div className="content-text">
              <h1>Let's Build</h1>
              <p>Your Gaming computer online now !</p>
-             <Link to='/Products'><button>BUY PRODUCTS</button></Link>
+             {/* <Link to='/Products'><button>BUY PRODUCTS</button></Link> */}
+             <button>BUY PRODUCTS</button>
           </div>
         </div>
     </div>
